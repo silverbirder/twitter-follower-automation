60ed767072fbae3ac246d4f640a9e6cede102d63
shibamoto <silverbirder@gmail.com>
2020-09-29 17:27:21 +0900

    😎 The implementation is complete. This is fine.

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
 # twitter-follower-automation
+
+## requirement
+
+* OAuth1
+  * Mb2Vpd5nfD3Pz-_a-39Q4VfxhMjh3Sh48
--- scripts.js
+++ scripts.js
@@ -1,8 +1,8 @@
-var getTwitterService = function () {
-    var consumerKey = PropertiesService.getScriptProperties().getProperty("TWITTER_CONSUMER_KEY");
-    var consumerSecret = PropertiesService.getScriptProperties().getProperty("TWITTER_CONSUMER_SECRET");
-    var accessToken = PropertiesService.getScriptProperties().getProperty("TWITTER_ACCESS_TOKEN");
-    var accessTokenSecret = PropertiesService.getScriptProperties().getProperty("TWITTER_ACCESS_TOKEN_SECRET");
+const getTwitterService = function () {
+    const consumerKey = PropertiesService.getScriptProperties().getProperty("TWITTER_CONSUMER_KEY");
+    const consumerSecret = PropertiesService.getScriptProperties().getProperty("TWITTER_CONSUMER_SECRET");
+    const accessToken = PropertiesService.getScriptProperties().getProperty("TWITTER_ACCESS_TOKEN");
+    const accessTokenSecret = PropertiesService.getScriptProperties().getProperty("TWITTER_ACCESS_TOKEN_SECRET");
     return OAuth1.createService("Twitter")
         .setAccessTokenUrl("https://api.twitter.com/oauth/access_token")
         .setRequestTokenUrl("https://api.twitter.com/oauth/request_token")
@@ -12,18 +12,21 @@ var getTwitterService = function () {
         .setAccessToken(accessToken, accessTokenSecret);
 };
 
-var myFunction = function() {
-    var service = getTwitterService();
+const myFunction = function () {
+    const service = getTwitterService();
 
-    var homeTimeline = service.fetch("https://api.twitter.com/1.1/statuses/home_timeline.json", { method:"GET" });
-    var homeTimelineJson = JSON.parse(homeTimeline.getContentText())
-    var homeTimelineUsers = homeTimelineJson.map(function(tweet){
+    // 1. accessToken発行者Twitterアカウントのタイムラインより直近のツイートをしたTwitterアカウントを抽出
+    const homeTimeline = service.fetch("https://api.twitter.com/1.1/statuses/home_timeline.json", {method: "GET"});
+    const homeTimelineJson = JSON.parse(homeTimeline.getContentText());
+    const homeTimelineUsers = homeTimelineJson.map(function (tweet) {
         return tweet.user;
-    })
-    var homeTimelineUser = homeTimelineUsers[0];
-    var followersList = service.fetch("https://api.twitter.com/1.1/followers/list.json?screen_name="+homeTimelineUser.screen_name, { method:"GET" });
-    var followersListJson = JSON.parse(followersList.getContentText()).users;
-    var keywords = [
+    });
+    const homeTimelineUser = homeTimelineUsers[0];
+
+    // 2. 1のフォロワーリストを抽出し、特定キーワードを持つTwitterアカウントで選別
+    const followersList = service.fetch("https://api.twitter.com/1.1/followers/list.json?screen_name=" + homeTimelineUser.screen_name, {method: "GET"});
+    const followersListJson = JSON.parse(followersList.getContentText()).users;
+    const keywords = [
         "react",
         "vue",
         "angular",
@@ -96,34 +99,39 @@ var myFunction = function() {
         "perl",
         "go",
         "sql",
+        "tdd",
+        "テスト",
+        "test"
     ];
-    var followersForMyTopic = followersListJson.filter(function(user){
+    const myName = PropertiesService.getScriptProperties().getProperty("TWITTER_MY_NAME");
+    const followersForMyTopic = followersListJson.filter(function (user) {
         Logger.log(user.description);
-        return user.description.match(new RegExp("("+keywords.join("|")+")", "ig")) !== null
-    })
-
-    var screenNames = followersForMyTopic.map(function(follower){
+        return user.description.match(new RegExp("(" + keywords.join("|") + ")", "ig")) !== null　&& user.screen_name != myName
+    });
+    const screenNames = followersForMyTopic.map(function (follower) {
         return follower.screen_name
-    }).join(",")
+    }).join(",");
     Logger.log(screenNames);
-    var friendshipsLookup = service.fetch("https://api.twitter.com/1.1/friendships/lookup.json?screen_name="+screenNames, { method:"GET" });
-    var friendshipsLookupJson = JSON.parse(friendshipsLookup.getContentText())
-    var willFollowList = friendshipsLookupJson.filter(function(friend){
+
+    // 3. 2が互いにフォローしていない関係であるか判定
+    const friendshipsLookup = service.fetch("https://api.twitter.com/1.1/friendships/lookup.json?screen_name=" + screenNames, {method: "GET"});
+    const friendshipsLookupJson = JSON.parse(friendshipsLookup.getContentText())
+    const willFollowList = friendshipsLookupJson.filter(function (friend) {
         Logger.log(friend);
         return friend.connections.indexOf("followed_by") == -1 && friend.connections.indexOf("following") == -1  // フォローしてもされてもいない
-    }).map(function(friend){
+    }).map(function (friend) {
         return friend.screen_name;
     });
     Logger.log(willFollowList);
 
-    var maxFriendshipsCreatePosts = 10;
-
-    willFollowList.map(function(screenName, index){
-        if(index+1 > maxFriendshipsCreatePosts) {
+    // 4. 3で判定可となるTwitterアカウントをフォロー
+    const maxFriendshipsCreatePosts = 10;
+    willFollowList.map(function (screenName, index) {
+        if (index + 1 > maxFriendshipsCreatePosts) {
             Logger.log("over 10 posts by friendships/create.json");
             return;
         }
         Logger.log("friendships/create.json :" + screenName + ", index:" + index);
-        service.fetch("https://api.twitter.com/1.1/friendships/create.json?screen_name="+screenName+"&follow=false", { method:"POST" });
+        service.fetch("https://api.twitter.com/1.1/friendships/create.json?screen_name=" + screenName + "&follow=false", {method: "POST"});
     });
-}
+};
